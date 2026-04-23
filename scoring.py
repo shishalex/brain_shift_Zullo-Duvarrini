e6a17b92a0a9ea4d5b5b22feb8bdf7727543681a
shishalex <alx8.ps4@gmail.com>
2026-04-23 08:58:06 +0200

    dev scoring.py

--- scoring.py
+++ scoring.py
@@ -1,1 +1,9 @@
 # Non importa pygame
+def apply_answer(score: int, is_correct: bool) -> int:
+    try:
+        if is_correct:
+            return score + 10
+        else:
+            return score - 5
+    except (TypeError, ValueError) as e:
+        print(f"An error occured: {e}")
