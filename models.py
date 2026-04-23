ccd175637d81e620d2388bf9c45016482f975c36
loris-duva <loris.duvarrini@gmail.com>
2026-04-23 08:46:20 +0200

    dev models

--- models.py
+++ models.py
@@ -1,1 +1,11 @@
-# Non importa pygame
+from dataclasses import dataclass
+
+
+@dataclass
+class Trial:
+    position: str
+    letter: str
+    number: int
+    expected_answer: bool
+    user_answer: bool | None = None
+    is_correct: bool = False
